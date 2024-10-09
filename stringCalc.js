9ea59a37e3a449599baa197b9c33d87b34ea7474
Prashant Kumar <prashantkum26@gmail.com>
2024-10-09 11:40:05 +0530

    Handle the sum, new lines and special characters

--- stringCalc.js
+++ stringCalc.js
@@ -4,6 +4,19 @@ const add = (stringsNums) => {
     const input = stringsNums.replace(/\s+/g, '');
     if (input === '') return 0;
 
+    const cleanedInput = stringsNums.replace(/[^\d,;\n-]+/g, ' ');
+    const numArray = cleanedInput.split(/[\s,*\n;]+/);
+    let result = 0;
+    for (let i = 0; i < numArray.length; i++) {
+        const val = numArray[i];
+        if (Number(val)) {
+            result += Number(val)
+        }
+    }
+    return result;
 }
+
+// const a = add("1,2;5\n\n\nr4******6");
+// console.log(a)
 
 module.exports = { add };
--- test/stringCalc.test.mjs
+++ test/stringCalc.test.mjs
@@ -9,4 +9,27 @@ describe('String Calculator', () => {
     it('should handle for empty string', () => {
         expect(add("")).to.equal(0);
     });
+
+    it('should return a number for a number string', () => {
+        expect(add("1")).to.equal(1);
+        expect(add("2")).to.equal(2);
+        expect(add("3")).to.equal(3);
+        expect(add("4")).to.equal(4);
+    });
+
+    it('should return the sum of two numbers', () => {
+        expect(add("1,2")).to.equal(3);
+    });
+
+    it('should return the sum of unknown length of numbers', () => {
+        expect(add('1,2,3,4,5,6,7,8,9')).to.equal(45);
+    });
+
+    it('should handle new lines between numbers', () => {
+        expect(add('1\n2,3')).to.equal(6);
+    });
+    
+    it('should handle customized delimiters', () => {
+        expect(add('//;\n1;2')).to.equal(3);
+    });
 });
